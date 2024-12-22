e56f36b77fcd41fb2db20fc5c7d3c9789cfe761a
MAziz <133257006+MuhammadAziz9@users.noreply.github.com>
2024-12-23 03:14:12 +0500

    Add files via upload

---
+++ js3/script.js
@@ -0,0 +1,32 @@
+let age = +prompt('Yoshingiz nechchida')
+// if (age > 18){
+//     alert('Saytimizga hush kelibsiz')
+// }else if (age>35){
+//     alert('Siz kech qoldiz')
+// }
+// else{
+//     alert('Bizning saytimiz sizga mos emas')
+// }
+// switch(age){
+//     case '18' : 
+//     alert('Sizning yoshingiz 18da')
+//     break
+//     case '50' : 
+//     alert('Sizning yoshingiz 50da')
+//     break
+//     case '28' : 
+//     alert('Sizning yoshingiz 50da')
+//     break
+//     default:
+//         alert('Siz gandonsiz')
+// // }
+
+
+// switch(age){
+//     case 18 : 
+//     alert('Sizning yoshingiz 18da')
+//     break
+//     default:
+//         alert('Siz gandonsiz')
+// }
+
