b742717c4a06679bf3fa4256daa687f4bfbd1050
MAziz <133257006+A1909bek@users.noreply.github.com>
2024-12-23 02:12:35 +0500

    Add files via upload

---
+++ js1/script.js
@@ -0,0 +1,20 @@
+let person = {
+    age : 19,
+    name : 'Azizbek',
+    car : 'Toyota Corrolla',
+    isMerried : false
+}
+
+console.log(person);
+
+let array = [19,'Azizbek',"Toyota Corolla",false]
+console.log(array);
+let answer = alert('Assalomu alaykum!')
+console.log(answer)
+
+let answer1 = confirm('Siz 18 yoshdan otkanmisiz')
+console.log(answer1);
+
+let answer2 = prompt('Ismingiz nima?')
+console.log(answer2);
+
